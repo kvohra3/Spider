bd9a6c483066d07c63a22e23200fda70dd02f3d4
Karan Vohra <karanvohra@Karans-MBP.fios-router.home>
2017-09-22 13:36:03 -0400

    list of <a>

--- server.js
+++ server.js
@@ -4,71 +4,78 @@ var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
 
-app.get('/', function(req, res){
-  url = 'https://www.reddit.com/r/Fantasy_Football/'
-  // url = 'http://www.imdb.com/title/tt1229340/'
+// app.get('/scrape', function(req, res){
+url = 'https://www.reddit.com/r/Fantasy_Football/';
+// url = 'http://www.imdb.com/title/tt1229340/'
 
   //Structre of request call:
   //1st param: URL
   //callback func -> 3 params: err, response status, and html
-  request(url, function(err, response, html) {
+request(url, function(err, response, html) {
 
     //first check no errors occured with request
     if(!err) {
-      console.log(html);
-
-    //   //cherrio -> jQuery func on html
-    //   var $ = cheerio.load(html);
-
-    //   //define variable to capture
-    //   var title, release, rating;
-    //   var json = {title: "", release: "", rating: ""};
-
-    //   //look for unique identifier
-    //   //e.x. <h1 class="header">
-    //   $('.header').filter(function() {
-
-    //     //store data into var so we can easily see whats up
-    //     var data = $(this);
-
-    //     //look at DOM and see that title is under first child element of header tag
-    //     //use jQuery to navigate and get text
-    //     title = data.children().first().text();
-
-    //     //release is last element
-    //     release = data.children().last().children().text();
-
-    //     //store data in json obj
-    //     json.title = title;
-    //     json.release = release;
-    //   })
-
-    //   //rating in a diff section so diff func
-    //   $('.star-box-giga-star').filter(function() {
-    //     var data = $(this);
-
-    //     //exactly what we need
-    //     rating = data.text();
-    //     json.rating = rating;
-    //   })
+      var $ = cheerio.load(html);
+      links = $('a'); //jquery get all hyperlinks
+      $(links).each(function(i, link){
+        console.log(i+') '+ $(link).text() + ': ' + $(link).attr('href'));
+        console.log(); 
+      });
     }
-
-    // // use fs module to write to system
-    // //writeFile fun -> 3 param:
-    //   //1) output.json - created filename
-    //   //2) json.stringify(json, null, 4) 
-    //     //data to write. extra step of calling stringify() to make JSON easier to read
-    //   //3) callback func - callback func to let us know status of function
-    // fs.writeFile('rawOutput.json', JSON.stringify(json, null, 4), function(err) {
-    //   console.log('Check projects folder yo');
-    // })
-
-    res.send('check your console!')
   })
-})
 
-app.listen('8081')
+      //cherrio -> jQuery func on html
+//       var $ = cherrio.load(html);
 
-console.log('Magic happens on port 8081');
+//       //define variable to capture
+//       var title, release, rating;
+//       var json = {title: "", release: "", rating: ""};
 
-exports = module.exports = app;
+//       //look for unique identifier
+//       //e.x. <h1 class="header">
+//       $('.header').filter(function() {
+
+//         //store data into var so we can easily see whats up
+//         var data = $(this);
+
+//         //look at DOM and see that title is under first child element of header tag
+//         //use jQuery to navigate and get text
+//         title = data.children().first().text();
+
+//         //release is last element
+//         release = data.children().last().children().text();
+
+//         //store data in json obj
+//         json.title = title;
+//         json.release = release;
+//       })
+
+//       //rating in a diff section so diff func
+//       $('.star-box-giga-star').filter(function() {
+//         var data = $(this);
+
+//         //exactly what we need
+//         rating = data.text();
+//         json.rating = rating;
+//       })
+//     }
+
+//     // use fs module to write to system
+//     //writeFile fun -> 3 param:
+//       //1) output.json - created filename
+//       //2) json.stringify(json, null, 4) 
+//         //data to write. extra step of calling stringify() to make JSON easier to read
+//       //3) callback func - callback func to let us know status of function
+//     fs.writeFile('rawOutput.json', JSON.stringify(json, null, 4), function(err) {
+//       console.log('Check projects folder yo');
+//     })
+
+//     res.send('check your console!')
+//   })
+// // })
+
+// app.listen('8081')
+
+// console.log('Magic happens on port 8081');
+
+// exports = module.exports = app;
